ffbd7b6c908a635ed734aa81e969d131434ad60b
SoniaMC97 <soniamarcas97@gmail.com>
2021-03-28 21:07:58 +0200

    Añadidio opciones

--- SenseHat_sonia.py
+++ SenseHat_sonia.py
@@ -18,26 +18,33 @@ class Aplicacion:
         self.list_max = [105, 1260, 100]   #Valores maximos
         self.periodo = self.periodo_inicial
 
-        #Creamos la ventana Monitorizacion
         self.ventana = tk.Tk() 
         self.ventana.title("Practica GUI SenseHat")
 
-        self.labelframe1=ttk.LabelFrame(self.ventana, text="Control")        
-        self.labelframe1.grid(column=0, row=1, padx=5, pady=10)        
-        self.control()
-
-        self.labelframe2=ttk.LabelFrame(self.ventana, text="Medidas")        
-        self.labelframe2.grid(column=0, row=2, padx=5, pady=10)        
-        self.medidas()
-        
-        self.labelframe3=ttk.LabelFrame(self.ventana, text="Historico")        
-        self.labelframe3.grid(column=0, row=3, padx=5, pady=10)        
-        self.historico()
+        #Creamos las opciones
+        menubar1 = tk.Menu(self.ventana)
+        self.ventana.config(menu=menubar1)
+        opciones1 = tk.Menu(menubar1)
+        opciones1.add_command(label="Configurar Periodo", command=self.cambiar_periodo)
+        menubar1.add_cascade(label="Opciones", menu=opciones1)  
 
 
+        #Creamos la ventana Monitorizacion
         self.cuaderno1 = ttk.Notebook(self.ventana)
         self.pagina1 = ttk.Frame(self.cuaderno1)
         self.cuaderno1.add(self.pagina1, text = "Monitorizacion")
+
+        self.labelframe1=ttk.LabelFrame(self.pagina1, text="Control")        
+        self.labelframe1.grid(column=0, row=1, padx=5, pady=10)        
+        self.control()
+
+        self.labelframe2=ttk.LabelFrame(self.pagina1, text="Medidas")        
+        self.labelframe2.grid(column=0, row=2, padx=5, pady=10)        
+        self.medidas()
+        
+        self.labelframe3=ttk.LabelFrame(self.pagina1, text="Historico")        
+        self.labelframe3.grid(column=0, row=3, padx=5, pady=10)        
+        self.historico()
 
         #Creamos la ventana Gráfico
         self.pagina2 = ttk.Frame(self.cuaderno1)
@@ -45,12 +52,20 @@ class Aplicacion:
 
         self.cuaderno1.grid(column = 0, row = 0, sticky='WE')
 
-        #Creamos las opciones
 
-        
 
+        #self.ventana.after(self.periodo, self.llamada_medir)
         self.ventana.mainloop()
-    
+
+    def cambiar_periodo(self):
+        dialogo1 = DialogoPeriodo(self.ventana)
+        self.periodo = dialogo1.modificar()
+        print("periodo: ", self.periodo)
+        self.label1_1=ttk.Label(self.labelframe1, text = str(self.periodo))
+        self.label1_1.grid(column=2, row=1)
+
+
+
     def control(self):
         #Crear boton
         self.boton=tk.Button(self.labelframe1, text="Parar")#, command=self.calcularcuadrado)
@@ -60,7 +75,7 @@ class Aplicacion:
         self.label1=ttk.Label(self.labelframe1, text="Periodo:")
         self.label1.grid(column=0, row=1)
 
-        self.label1_1=ttk.Label(self.labelframe1, )
+        self.label1_1=ttk.Label(self.labelframe1, text = str(self.periodo))
         self.label1_1.grid(column=2, row=1)
 
     def medidas(self):
@@ -109,5 +124,33 @@ class Aplicacion:
 
         #self.boton3=tk.Button(self.labelframe3, text="Exportar")#, command=self.calcularcuadrado)
         #self.boton3.grid(column=0, row=1)
+
+    def llamada_medir(self):
+        print("prueba")
+
+class DialogoPeriodo:
+    def __init__(self, ventanaprincipal):
+        self.dialogo=tk.Toplevel(ventanaprincipal)
+        self.label1=ttk.Label(self.dialogo, text="Indique Periodo:")
+        self.label1.grid(column=0, row=0, padx=5, pady=5)
+
+        self.dato1=tk.StringVar()
+        self.entry1=ttk.Entry(self.dialogo, textvariable=self.dato1)
+        self.entry1.grid(column=1, row=0, padx=5, pady=5)
+        self.entry1.focus()
+       
+        self.boton1=ttk.Button(self.dialogo, text="Confirmar", command=self.confirmar)
+        self.boton1.grid(column=1, row=2, padx=5, pady=5)
+        self.dialogo.protocol("WM_DELETE_WINDOW", self.confirmar)
+        self.dialogo.resizable(0,0)
+        self.dialogo.grab_set()
+
+    def modificar(self):
+        self.dialogo.wait_window()
+        return self.dato1.get()
+
+    def confirmar(self):
+        self.dialogo.destroy()
+    
 
 aplicacion1 = Aplicacion()
